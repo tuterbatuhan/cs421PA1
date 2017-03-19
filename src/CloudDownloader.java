6e0734ff196d379e88260d4d9d24013b3b1071d6
Batuhan <tuterbatuhan@gmail.com>
2017-03-19 17:39:17 +0300

    done

--- src/CloudDownloader.java
+++ src/CloudDownloader.java
@@ -1,6 +1,4 @@
-//
-//
-// http://www.avajava.com/tutorials/lessons/how-do-i-connect-to-a-url-using-basic-authentication.html
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -17,10 +15,8 @@ public class CloudDownloader
 
 	public static void main(String[] args) throws IOException
 	{
-		String urlString = args[0];
-		String userName = args[1];
-		String password = args[2];
-		String authString = userName + ":" + password;
+		String urlString = "http://"+args[0];
+		String authString = args[1];
 		String asB64 = Base64.getEncoder().encodeToString(authString.getBytes());
 		URL url = new URL(urlString);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -38,21 +34,22 @@ public class CloudDownloader
 		FileOutputStream fos=null;
 		int totalBytes=0;
 		Boolean finished = false;
+		
 		if((inputLine = in.readLine()) != null)
 			file = new File(inputLine);
 		if((inputLine = in.readLine()) != null)
 			totalBytes = Integer.parseInt(inputLine);
-		//System.out.println(totalBytes);
+		
 		int byteCounter=0;
 		fos = new FileOutputStream(file);
 		while(!finished){
-			String urlString2 = in.readLine();
+			String urlString2 = "http://"+in.readLine();
 			String asB642 = Base64.getEncoder().encodeToString(in.readLine().getBytes());
 			Scanner s = new Scanner(in.readLine());
 			s.useDelimiter("-");
 			int bytesFrom = s.nextInt();
 			int bytesTo = s.nextInt();
-			URL url2 = new URL(urlString);
+			URL url2 = new URL(urlString2);
 			HttpURLConnection connection2 = (HttpURLConnection) url2.openConnection();
 			connection2.setRequestMethod("GET");
 			connection2.setRequestProperty("Authorization", "Basic " + asB642);
@@ -62,9 +59,7 @@ public class CloudDownloader
 				System.out.println("Response Code 2 : " + responseCode2);
 				return;
 			}
-			
 			byteCounter = bytesTo;
-			
 			InputStream inputStream = connection2.getInputStream();
 			int data = inputStream.read();
 			while(data != -1) {
@@ -81,8 +76,7 @@ public class CloudDownloader
 			response.append(inputLine+	"\n");
 		}
 		in.close();
-		System.out.println(response.toString());
-		
+		System.out.println("Done");
 	}
 
 }
